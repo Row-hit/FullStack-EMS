1c81db645e0840e3b7d1e909fa1d40fe73d713ac
ROHIT RAJPUT <rohit.rajput.coder@gmail.com>
2026-04-18 09:10:07 +0530

    First Commit

---
+++ Client/src/app.route.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="text-2xl bg-red-500">HEllooo World</div>,
+  },
+]);
+
+export default router;
